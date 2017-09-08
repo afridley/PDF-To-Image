5e625d947d02a6272c38fc0a0847c671bb60000a
Andrew Fridley <ajhfridley@gmail.com>
2017-09-07 22:44:59 -0500

    Adding Button & Accepted / Rejected lists

--- src/app.js
+++ src/app.js
@@ -1,35 +1,62 @@
-import React from 'react';
-import Dropzone from 'react-dropzone'
+import React from 'react';;
+import Dropzone from 'react-dropzone';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import Button from 'material-ui/Button';
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiButton: {
+      // Name of the styleSheet
+      root: {
+        // Name of the rule
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+      },
+    },
+  },
+});
 export default class App extends React.Component {
-  constructor() {
+    constructor() {
     super()
-    this.state = { files: [] }
-  }
-
-  onDrop(files) {
-    this.setState({
-      files
-    });
+    this.state = {
+      accepted: [],
+      rejected: []
+    }
   }
 
   render() {
     return (
       <section>
         <div className="container">
-          <Dropzone 
-          className="dropzone"
-          accept="application/pdf"
-          style={{}}
-          onDrop={this.onDrop.bind(this)}> 
-         <div className="item">Drop PDF's Here</div>          
+          <Dropzone
+            className="dropzone"
+            accept="application/pdf"
+            onDrop={(accepted, rejected) => { this.setState({ accepted, rejected }); }}
+          >
+           <div className="item">Drop PDF's</div> 
           </Dropzone>
         </div>
+        <div className="bContainer">
+         <MuiThemeProvider theme={theme}>
+      <Button>{'Convert'}</Button>
+    </MuiThemeProvider>
+        </div>
         <aside className="mBelow">
-          <h2>Dropped files</h2>
+          <h2>Accepted</h2>
           <ul>
             {
-              this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              this.state.accepted.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+            }
+          </ul>
+          <h2>Rejected</h2>
+          <ul>
+            {
+              this.state.rejected.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
             }
           </ul>
         </aside>
