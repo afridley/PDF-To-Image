0f3eb7485e3c200af72d7ee0937077fe39bf422b
Andrew Fridley <ajhfridley@gmail.com>
2017-09-18 13:39:29 -0500

    Ux Improvements

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import{
   close,
   minimize
 } from '../src/utils/functions';
+import Modal from '../src/components/modal';
 
 var fs = require('fs');
 var gm = require('gm');
@@ -35,9 +36,17 @@ export default class App extends React.Component {
     super()
     this.state = {
       acceptedFiles: [],
-      rejectedFiles: []
+      rejectedFiles: [],
+      modalActive:false
     }
   }
+
+  openModal(){
+  this.setState({modalActive:true})
+}
+  closeModal(){
+  this.setState({modalActive:false})
+}
 
 onDrop(acceptedFiles, rejectedFiles) {
   acceptedFiles.forEach(file => {
@@ -50,7 +59,7 @@ onDrop(acceptedFiles, rejectedFiles) {
     var writeStream = fs.createWriteStream(file.path + ".png");
     stdout.pipe(writeStream);
     if (err) {
-      console.log('well shit')
+      console.log('Error')
     }
     console.log(file.path);
   });
@@ -74,14 +83,44 @@ onDrop(acceptedFiles, rejectedFiles) {
             accept="application/pdf"
             onDrop={this.onDrop.bind(this)}            
           >
-           <div className="item">Drop PDF's</div> 
+           <div className="item">Drop PDF's 
+           </div> 
           </Dropzone>
         <div className="bContainer">
           {/* Custom Button Theme */}
          <MuiThemeProvider theme={theme}>
-            <Button>{'Tutorial'}</Button>
-         </MuiThemeProvider>
+            <Button onClick={() => this.openModal()}>{'Tutorial'}</Button>
+            </MuiThemeProvider> 
         </div>
+        <div>
+          <Modal isOpen = {this.state.modalActive} onClose = {() => this.closeModal()}> 
+            <div className="modal-text">  
+            <h1>Tutorial</h1>
+            <p>This program requires Graphics Magick</p>
+
+          <h3>Linux</h3>
+          <p>Graphics Magick may already be installed
+            <p>Open terminal</p>
+            <p><code>sudo apt-get install graphicsmagick</code></p>
+          </p>
+
+          <h3>Windows</h3>
+          <p>Download the latest version from</p>
+          <p><a href="ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/windows/">ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/windows/</a></p>
+
+          <h3>Mac</h3>
+          <p>Open terminal (cmd + space) </p>
+          <p><code>brew install graphicsmagick</code></p>
+
+          <h3>How To Use</h3>
+          <p>Drag your .pdf files onto the drop area this will convert them to .png files in the same folder that they where dragged from.</p>
+
+         <MuiThemeProvider theme={theme}>
+            <Button onClick={() => this.closeModal()}>{'Close'}</Button>
+            </MuiThemeProvider> 
+            </div>
+          </Modal>  
+        </div>  
       </section>
     );
   }
